c5401ced1fabe3630c891f86c1023b9bade6b737
Dawone Holloway <hollowde.hollowde@mail.uc.edu>
2025-02-14 23:01:16 -0500

    changing stuff

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 echo "hello $@"
 echo "answer=42" >> $GITHUB_OUTPUT
 echo "### Hello $@! :rocket:" >> $GITHUB_STEP_SUMMARY
-echo "<h3> THe answer from Deep Thought is 42 :robot:</h3>" >> $GITHUB_STEP_SUMMARY
+echo "<h3> THe answer from Deep Thought is 7 :robot:</h3>" >> $GITHUB_STEP_SUMMARY
